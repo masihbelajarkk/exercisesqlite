4ce18093b4609ddd3deceb31efc1c5d4ef7a8b98
Gaffar Nazarudin Kamal <49097493+masihbelajarkk@users.noreply.github.com>
2020-04-16 23:54:12 +0700

    Add files via upload
    Final

---
+++ exerciseSQLlite/app/src/main/java/com/example/exercisesql_lite/ModKontak.java
@@ -0,0 +1,49 @@
+package com.example.exercisesql_lite;
+
+public class ModKontak {
+    private String idUser;
+    private String nama;
+    private String phone;
+    private String email;
+    private String alamat;
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
